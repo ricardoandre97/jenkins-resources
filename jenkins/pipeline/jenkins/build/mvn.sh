69dd5f287973c617b9abe821bae2997b9e3f2166
Ricardo Gonzalez <ricardo.andre.gonzalez07@gmail.com>
2025-03-20 18:23:24 +0800

    Update mvn.sh

--- jenkins/pipeline/jenkins/build/mvn.sh
+++ jenkins/pipeline/jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.9 "$@"
